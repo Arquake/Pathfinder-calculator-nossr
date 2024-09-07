98b937bf13ff313e5089571740e10f8af10ed659
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-09-07 21:15:49 +0200

    Init Push
    pathfinder calculator sans ssr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {EncounterCalculatorComponent} from "./encounter-calculator/encounter-calculator.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: EncounterCalculatorComponent,
+    title: 'Pathfind Encounter Calculator',
+    data: {
+
+      description: 'A simple Pathfinder 2E encounter calculator to create encounter relative to player\'s level',
+    }
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  },
+];
